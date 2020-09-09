e37e5c3d308a8a28fdaba982c52d83bad13d97e4
Doktor-Doom <andre.o.martin@gmail.com>
2020-09-09 06:44:58 -0700

    wrote out constructor then converted to class ES2015

--- js/app.js
+++ js/app.js
@@ -9,3 +9,15 @@ $.ajax('data/page-1.json', {method: 'GET', dataType: 'JSON' })
     })
   })
   .then(() => Horn.dropDown());
+
+class Horn {
+  constructor(object) {
+    this.imgUrl = object.image_url;
+    this.title = object.title;
+    this.description = object.description;
+    this.keyword = object.keyword;
+    this.horns = object.horns;
+    hornArr.push(this);
+  }
+}
+
